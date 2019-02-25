c6cbdeed478d11159cb39710061093c946a024db
Elizaveta <eliz.vitebsk@gmail.com>
2019-02-25 14:26:17 +0300

    ok

--- lab1/lab1/lab1/Source.cpp
+++ lab1/lab1/lab1/Source.cpp
@@ -5,11 +5,11 @@
 using namespace std;
 
 const unsigned int a_start = 16387, M = 2147483648, N = 1000, K = 48;
-const double e = 0.05, critical_value_kolmogorov = 1.36, critical_value_pirson = 18.307, k = 11;
+const double e = 0.05, critical_value_kolmogorov = 1.36, critical_value_pirson =  16.92, k = 10;
 
 void mkm(vector<double> &a, unsigned int beta)
 {
-	vector<unsigned int> az(N + K);
+	vector<unsigned int> az(N);
 	az[0] = beta * beta; 
 	a[0] = az[0] / (double)M;
 	for (int i = 1; i < a.size(); i++)
@@ -26,7 +26,7 @@ double Kolmogorov(vector<double> a)
 	for (int i = 0; i < N; i++) {
 		D = max(D, fabs(((double)(i + 1) / (double)N) - a[i]));
 	}
-	return D /** sqrt(N)*/;
+	return D * sqrt(N);
 }
 
 double Pirson(vector<double> a)
@@ -35,6 +35,7 @@ double Pirson(vector<double> a)
 	double i = 1.0 / k;
 	int j = 0, cnt = 0;
 	double X = 0;
+	int temp = 0;
 	while (i <= 1)
 	{
 		while (j < N && a[j] < i)
@@ -42,16 +43,16 @@ double Pirson(vector<double> a)
 			j++;
 			cnt++;
 		}
-		X += (((double)cnt - (double)N / k) * ((double)cnt - (double)N / k) / ((double)N / k));
-		cnt = 0;
+		X += (((double)cnt - (double)N / (double)k) * ((double)cnt - (double)N / (double)k) / ((double)N / (double)k));	
 		i += (1.0 / (double)k);
+		cnt = 0;
 	}
 	return X;
 }
 
 int main()
 {
-	vector<double> b(N + K, 0);
+	vector<double> b(N, 0);
 	mkm(b, a_start);
 	
 	double res_check = Kolmogorov(b);
@@ -68,23 +69,18 @@ int main()
 
 	vector<double> c(N, 0);
 	mkm(c, a_start * 2 + 1); 
-	res_check = Pirson(c);
-	cout << "Check Pirson first\n" << res_check << "\n";
-	if (res_check < critical_value_pirson)
-		cout << "ok\n";
-	else cout << "failed\n";
 
 	vector<double> v(K);
 	for (int i = 0; i < K; i++)
 		v[i] = b[i]; 
 
 	//McLaren - Marsaly
-	vector<double> a(N + K, 0);
+	vector<double> a(N, 0);
 	for (int i = 0; i < N; i++)
 	{
 		int s = (int)(c[i] * (double)K);
 		a[i] = v[s]; 
-		v[s] = b[(i + K) % M];
+		v[s] = b[(i + K) % N];
 	}
 	
 	res_check = Kolmogorov(a);
